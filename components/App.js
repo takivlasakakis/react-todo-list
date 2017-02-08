a20d9355d81a3d8d49e47a50bf53fdf27fdd5230
Taki Vlasakakis <takivlasakakis@TVlasakakis-MacBook-Pro.local>
2017-02-08 11:08:55 -0700

    adding to textinput and created textdisplay

--- components/App.js
+++ components/App.js
@@ -1,10 +1,18 @@
-import React, { Component } from 'react'
+import React, {
+	Component
+} from 'react'
+import TextInput from './TextInput'
 
 class App extends Component {
 
-  render() {
-    return <div>This is definitely a hot (module reloading) React app now!</div>
-  }
+	render() {
+		return (
+			<div>
+				<h1> This is the App Component </h1>
+				<TextInput/>
+			</div>
+		)
+	}
 
 }
 
